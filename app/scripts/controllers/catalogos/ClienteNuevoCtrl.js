c2cf5445cb3977956453f1152a1c16b5fed3a53c
alesabas <alejandro_jimenez24@hotmail.com>
2017-08-12 14:03:37 -0500

    Update

--- app/scripts/controllers/catalogos/ClienteNuevoCtrl.js
+++ app/scripts/controllers/catalogos/ClienteNuevoCtrl.js
@@ -2,9 +2,38 @@
 
 angular
     .module('softvApp')
-    .controller('ClienteNuevoCtrl', function($scope){
+    .controller('ClienteNuevoCtrl', function(CatalogosFactory){
+
+        function initData(){
+            CatalogosFactory.GetPlazaList().then(function(data){
+                vm.PlazaList = data.GetPlazaListResult;
+            });
+
+            CatalogosFactory.GetPeriodoCobroList().then(function(data){
+                vm.PeriodoList = data.GetPeriodoCobroListResult;
+            });
+
+            /*CatalogosFactory.GetTipoClienteList_WebSoftvnew().then(function(data){
+                console.log(data);
+            });*/
+            
+        }
+
+        function AddDatosPersonales(){
+            console.log('OK');
+        }
+
+        function ValidateYearVen(){
+            if(vm.YearVen > 2016){
+
+            }
+        }
 
         var vm = this;
-        vm.TipoPersona = 'F';
+        vm.TipoPersona = "1";
+        vm.ValidateRFC = /^[A-Z]{4}\d{6}[A-Z]{3}$|^[A-Z]{4}\d{6}\d{3}$|^[A-Z]{4}\d{6}[A-Z]{2}\d{1}$|^[A-Z]{4}\d{6}[A-Z]{1}\d{2}$|^[A-Z]{4}\d{6}\d{2}[A-Z]{1}$|^[A-Z]{4}\d{6}\d{1}[A-Z]{2}$/;
+        vm.AddDatosPersonales = AddDatosPersonales;
+        vm.ValidateYearVen = ValidateYearVen;
+        initData();
         
     });
--- app/scripts/factories/CatalogosFactory.js
+++ app/scripts/factories/CatalogosFactory.js
@@ -2,11 +2,76 @@
 
 angular
     .module('softvApp')
-    .factory('CatalogosFactory', function(){
+    .factory('CatalogosFactory', function($http, $q, globalService, $localStorage){
 
         var factory = {};
-        
-        var paths = {};
+        var paths = {
+            GetPlazaList: '/Plaza/GetPlazaList',
+            GetPeriodoCobroList: '/PeriodoCobro/GetPeriodoCobroList',
+            GetTipoClienteList_WebSoftvnew: '/TipoCliente/GetTipoClienteList_WebSoftvnew',
+            AddClienteL: '/Cliente/AddClienteL'
+        };
+
+        factory.GetPlazaList = function(){
+            var deferred = $q.defer();
+            var config = {
+                headers: {
+                    'Authorization': $localStorage.currentUser.token
+                }
+            };
+            $http.get(globalService.getUrl() + paths.GetPlazaList, config).then(function (response) {
+                deferred.resolve(response.data);
+            }).catch(function (response) {
+                deferred.reject(response);
+            });
+            return deferred.promise;
+        };
+
+        factory.GetPeriodoCobroList = function(){
+            var deferred = $q.defer();
+            var config = {
+                headers: {
+                    'Authorization': $localStorage.currentUser.token
+                }
+            };
+            $http.get(globalService.getUrl() + paths.GetPeriodoCobroList, config).then(function (response) {
+                deferred.resolve(response.data);
+            }).catch(function (response) {
+                deferred.reject(response);
+            });
+            return deferred.promise;
+        };
+
+        factory.GetTipoClienteList_WebSoftvnew = function(){
+            var deferred = $q.defer();
+            var config = {
+                headers: {
+                    'Authorization': $localStorage.currentUser.token
+                }
+            };
+            $http.get(globalService.getUrl() + paths.GetTipoClienteList_WebSoftvnew, config).then(function (response) {
+                deferred.resolve(response.data);
+            }).catch(function (response) {
+                deferred.reject(response);
+            });
+            return deferred.promise;
+        };
+
+        factory.AddClienteL = function (ObjCliente) {
+            var deferred = $q.defer();
+            var config = {
+                headers: {
+                    'Authorization': $localStorage.currentUser.token
+                }
+            };
+            var parametros = {};
+            $http.post(globalService.getUrl() + paths.AddClienteL, JSON.stringify(parametros), config).then(function (response) {
+                deferred.resolve(response.data);
+            }).catch(function (response) {
+                deferred.reject(response);
+            });
+            return deferred.promise;
+        };
 
         return factory;
 
