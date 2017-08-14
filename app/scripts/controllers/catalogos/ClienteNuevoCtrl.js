0db7982e3af55c0f9a3c189acdcf99758cba293b
alesabas <alejandro_jimenez24@hotmail.com>
2017-08-14 17:34:55 -0500

    Datos Personales y Postales

--- app/scripts/controllers/catalogos/ClienteNuevoCtrl.js
+++ app/scripts/controllers/catalogos/ClienteNuevoCtrl.js
@@ -13,27 +13,65 @@ angular
                 vm.PeriodoList = data.GetPeriodoCobroListResult;
             });
 
-            /*CatalogosFactory.GetTipoClienteList_WebSoftvnew().then(function(data){
-                console.log(data);
-            });*/
-            
+            CatalogosFactory.GetTipoClienteList_WebSoftvnew().then(function(data){
+                vm.TipoCobroList = data.GetTipoClienteList_WebSoftvnewResult;
+            });
+
+            CatalogosFactory.GetEstadoList2_web().then(function(data){
+                vm.EstadoList = data.GetEstadoList2_webResult;
+            });
         }
 
         function AddDatosPersonales(){
-            console.log('OK');
+            var FechaNacD = vm.FechaNac.getDate();
+            var FechaNacM = vm.FechaNac.getMonth() + 1;
+            var FechaNacY = vm.FechaNac.getFullYear();
+            var ObjCliente = {};
+            ObjCliente.Nombre = vm.Nombre;
+            ObjCliente.NombreAdi = vm.NombreAdi;
+            ObjCliente.PrimerApe = vm.PrimerApe;
+            ObjCliente.SegundoApe = vm.SegundoApe;
+            ObjCliente.ClaveElector = vm.ClaveElector;
+            ObjCliente.Telefono = vm.Telefono;
+            ObjCliente.Celular = vm.Celular;
+            ObjCliente.Email = vm.Email;
+            ObjCliente.IdPlaza = vm.Plaza.IdPlaza;
+            ObjCliente.IdPeriodo = vm.Periodo.IdPeriodo;
+            ObjCliente.IdTipoCliente = vm.TipoCobro.IdTipoCliente;
+            ObjCliente.TipoPersona = (vm.TipoPersona == 'F') ? 1 : 0;
+            ObjCliente.FechaNac = FechaNacD + '/' + FechaNacM + '/' + FechaNacY;
+            CatalogosFactory.AddClienteL(ObjCliente).then(function(data){
+                var IdContratoCliente = data.AddClienteLResult;
+                GetDatosClientes(IdContratoCliente);
+            });
         }
 
-        function ValidateYearVen(){
-            if(vm.YearVen > 2016){
+        function GetDatosClientes(IdContratoCliente){
+            CatalogosFactory.GetDeepCliente(IdContratoCliente).then(function(data){
+                console.log(data);
+                vm.IdContrato = data.GetDeepClienteResult.GetDeepClienteResult;
+            });
+        }
 
-            }
+        function GetCiudadMunicipio(){
+            CatalogosFactory.GetEstadosRelMun(vm.Estado.IdEstado).then(function(data){
+                console.log(data);
+                vm.GetCiudadMunicipioList = data.GetEstadosRelMunResult;
+            })
+        }
+
+        function GetLocalidad(){
+            CatalogosFactory.GetLocalidadRelMun(vm.Municipio.IdMunicipio).then(function(data){
+                console.log(data)
+            });
         }
 
         var vm = this;
         vm.TipoPersona = "1";
         vm.ValidateRFC = /^[A-Z]{4}\d{6}[A-Z]{3}$|^[A-Z]{4}\d{6}\d{3}$|^[A-Z]{4}\d{6}[A-Z]{2}\d{1}$|^[A-Z]{4}\d{6}[A-Z]{1}\d{2}$|^[A-Z]{4}\d{6}\d{2}[A-Z]{1}$|^[A-Z]{4}\d{6}\d{1}[A-Z]{2}$/;
+        vm.MesList = {};
         vm.AddDatosPersonales = AddDatosPersonales;
-        vm.ValidateYearVen = ValidateYearVen;
+        vm.GetCiudadMunicipio = GetCiudadMunicipio;
         initData();
         
     });
--- app/scripts/factories/CatalogosFactory.js
+++ app/scripts/factories/CatalogosFactory.js
@@ -9,7 +9,13 @@ angular
             GetPlazaList: '/Plaza/GetPlazaList',
             GetPeriodoCobroList: '/PeriodoCobro/GetPeriodoCobroList',
             GetTipoClienteList_WebSoftvnew: '/TipoCliente/GetTipoClienteList_WebSoftvnew',
-            AddClienteL: '/Cliente/AddClienteL'
+            AddClienteL: '/Cliente/AddClienteL',
+            GetDeepCliente: '/Cliente/GetDeepCliente',
+            GetEstadoList2_web: '/Estado/GetEstadoList2_web',
+            GetEstadosRelMun: '/RelMunicipioEst/GetEstadosRelMun',
+            GetLocalidadRelMun: '/RelLocalidadMunEst/GetLocalidadRelMun',
+            GetColoniaRelLoc: '/RelColoniaLocMunEst/GetColoniaRelLoc',
+            GetCalleRelCol: '/RelCalleColonia/GetCalleRelCol'
         };
 
         factory.GetPlazaList = function(){
@@ -64,8 +70,129 @@ angular
                     'Authorization': $localStorage.currentUser.token
                 }
             };
-            var parametros = {};
-            $http.post(globalService.getUrl() + paths.AddClienteL, JSON.stringify(parametros), config).then(function (response) {
+            var Parametros = {
+                'lstCliente': {
+                    Nombre1Er: ObjCliente.Nombre,
+                    Nombre2Do: ObjCliente.NombreAdi,
+                    ApePaterno: ObjCliente.PrimerApe,
+                    ApeMaterno: ObjCliente.SegundoApe,
+                    ClvElector: ObjCliente.ClaveElector,
+                    Telefono: ObjCliente.Telefono,
+                    Celular: ObjCliente.Celular,
+                    Email: ObjCliente.Email,
+                    IdPlaza: ObjCliente.IdPlaza,
+                    IdPeriodo: ObjCliente.IdPeriodo,
+                    IdTipoCliente: ObjCliente.IdTipoCliente,
+                    EsPersonaFisica: ObjCliente.TipoPersona,
+                    FechaNacimiento: ObjCliente.FechaNac
+                }
+            };
+            $http.post(globalService.getUrl() + paths.AddClienteL, JSON.stringify(Parametros), config).then(function (response) {
+                deferred.resolve(response.data);
+            }).catch(function (response) {
+                deferred.reject(response);
+            });
+            return deferred.promise;
+        };
+
+        factory.GetDeepCliente = function (IdContrato) {
+            var deferred = $q.defer();
+            var config = {
+                headers: {
+                    'Authorization': $localStorage.currentUser.token
+                }
+            };
+            var Parametros = {
+                'IdContrato': IdContrato
+            };
+            $http.post(globalService.getUrl() + paths.GetDeepCliente, JSON.stringify(Parametros), config).then(function (response) {
+                deferred.resolve(response.data);
+            }).catch(function (response) {
+                deferred.reject(response);
+            });
+            return deferred.promise;
+        };
+        
+        factory.GetEstadoList2_web = function(){
+            var deferred = $q.defer();
+            var config = {
+                headers: {
+                    'Authorization': $localStorage.currentUser.token
+                }
+            };
+            $http.get(globalService.getUrl() + paths.GetEstadoList2_web, config).then(function (response) {
+                deferred.resolve(response.data);
+            }).catch(function (response) {
+                deferred.reject(response);
+            });
+            return deferred.promise;
+        };
+
+        factory.GetEstadosRelMun = function (IdEstado) {
+            var deferred = $q.defer();
+            var config = {
+                headers: {
+                    'Authorization': $localStorage.currentUser.token
+                }
+            };
+            var Parametros = {
+                'IdEstado': IdEstado
+            };
+            $http.post(globalService.getUrl() + paths.GetEstadosRelMun, JSON.stringify(Parametros), config).then(function (response) {
+                deferred.resolve(response.data);
+            }).catch(function (response) {
+                deferred.reject(response);
+            });
+            return deferred.promise;
+        };
+
+        factory.GetLocalidadRelMun = function (IdMunicipio) {
+            var deferred = $q.defer();
+            var config = {
+                headers: {
+                    'Authorization': $localStorage.currentUser.token
+                }
+            };
+            var Parametros = {
+                'IdMunicipio': IdMunicipio
+            };
+            $http.post(globalService.getUrl() + paths.GetLocalidadRelMun, JSON.stringify(Parametros), config).then(function (response) {
+                deferred.resolve(response.data);
+            }).catch(function (response) {
+                deferred.reject(response);
+            });
+            return deferred.promise;
+        };
+
+        factory.GetColoniaRelLoc = function (IdLocalidad) {
+            var deferred = $q.defer();
+            var config = {
+                headers: {
+                    'Authorization': $localStorage.currentUser.token
+                }
+            };
+            var Parametros = {
+                'IdLocalidad': IdLocalidad
+            };
+            $http.post(globalService.getUrl() + paths.GetColoniaRelLoc, JSON.stringify(Parametros), config).then(function (response) {
+                deferred.resolve(response.data);
+            }).catch(function (response) {
+                deferred.reject(response);
+            });
+            return deferred.promise;
+        };
+
+        factory.GetCalleRelCol = function (IdColonia) {
+            var deferred = $q.defer();
+            var config = {
+                headers: {
+                    'Authorization': $localStorage.currentUser.token
+                }
+            };
+            var Parametros = {
+                'IdColonia': IdColonia
+            };
+            $http.post(globalService.getUrl() + paths.GetCalleRelCol, JSON.stringify(Parametros), config).then(function (response) {
                 deferred.resolve(response.data);
             }).catch(function (response) {
                 deferred.reject(response);
