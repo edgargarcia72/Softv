442b6b97e135bb4bb605adec2d919ea9b4832b6f
alesabas <alejandro_jimenez24@hotmail.com>
2017-09-02 13:51:39 -0500

    Ventana Distribuidor, Validaciones

--- app/scripts/controllers/catalogos/ClienteNuevoCtrl.js
+++ app/scripts/controllers/catalogos/ClienteNuevoCtrl.js
@@ -271,7 +271,7 @@ angular
         vm.BlockInput = false;
         vm.TipoPersona = "F";
         vm.Title = 'Cliente nuevo';
-        vm.ValidateRFC = /^[A-Z]{4}\d{6}[A-Z]{3}$|^[A-Z]{4}\d{6}\d{3}$|^[A-Z]{4}\d{6}[A-Z]{2}\d{1}$|^[A-Z]{4}\d{6}[A-Z]{1}\d{2}$|^[A-Z]{4}\d{6}\d{2}[A-Z]{1}$|^[A-Z]{4}\d{6}\d{1}[A-Z]{2}$/;
+        vm.ValidateRFC = /^[A-Z]{4}\d{6}[A-Z]{3}$|^[A-Z]{4}\d{6}\d{3}$|^[A-Z]{4}\d{6}[A-Z]{2}\d{1}$|^[A-Z]{4}\d{6}[A-Z]{1}\d{2}$|^[A-Z]{4}\d{6}\d{2}[A-Z]{1}$|^[A-Z]{4}\d{6}\d{1}[A-Z]{2}$|^[A-Z]{4}\d{6}\d{1}[A-Z]{1}\d{1}$|^[A-Z]{4}\d{6}[A-Z]{1}\d{1}[A-Z]{1}$/;
         vm.MesList = [
             { IdMes: 1, Nombre: 'Enero' },
             { IdMes: 2, Nombre: 'Febrero' },
