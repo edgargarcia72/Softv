3bb59635ce1a23c882e7cb19e6cea1855f5c9d4f
EricChavez <erictics@gmail.com>
2017-09-05 17:41:09 -0500

    agregando catalogo de usuarios

--- app/scripts/controllers/configuracion/Usuarios/nuevoUsuarioCtrl.js
+++ app/scripts/controllers/configuracion/Usuarios/nuevoUsuarioCtrl.js
@@ -1,9 +1,83 @@
 'use strict';
 angular
   .module('softvApp')
-  .controller('nuevoUsuarioCtrl', function ($state, usuarioFactory, globalService, $uibModal) {
+  .controller('nuevoUsuarioCtrl', function ($state, usuarioFactory, globalService, $uibModal, $filter, rolFactory, encuestasFactory) {
+
+    this.$onInit = function () {
+      rolFactory.GetRolList().then(function (data) {
+        vm.Roles = data.GetRolListResult;
+        usuarioFactory.GetConsultaIdentificacionUsuario(0, '').then(function (result) {
+          console.log(result);
+          vm.Indentificaciones = result.GetConsultaIdentificacionUsuarioResult;
+
+          encuestasFactory.GetMuestra_DistribuidoresEncList().then(function (data) {
+            console.log(data.GetMuestra_DistribuidoresEncListResult);
+            vm.distribuidores = data.GetMuestra_DistribuidoresEncListResult;
+          });
+        });
+      });
+    };
+
+    function muestraplazas() {
+      encuestasFactory.Muestra_PlazaEnc(vm.distribuidor.Clv_Plaza).then(function (data) {
+        console.log(data.GetMuestra_PlazaEncListResult);
+        vm.plazas = data.GetMuestra_PlazaEncListResult;
+      });
+
+    }
+    
+    function agregaRelacion(){
+
+
+    }
+
+
+
+    function Guardar() {
+
+      var Parametros = {
+        'Clave': 0,
+        'Clv_Usuario': vm.Clave,
+        'Domicilio': '',
+        'Colonia': '',
+        'FechaIngreso': $filter('date')(vm.fechaingreso, 'dd/MM/yyyy'),
+        'FechaSalida': $filter('date')(vm.fechabaja, 'dd/MM/yyyy'),
+        'Activo': vm.activo,
+        'Pasaporte': vm.pass2,
+        'Clv_TipoUsuario': vm.rol.IdRol,
+        'CATV': false,
+        'Facturacion': true,
+        'Boletos': false,
+        'Mizar_AN': 0,
+        'RecibeMensaje': vm.recibemensaje,
+        'NotaDeCredito': 0,
+        'Clv_IdentificacionUsuario': vm.identificacion.Clave,
+        'RecibeMensajeDocumentos': 0,
+        'Nombre': vm.Nombre
+      };
+
+      usuarioFactory.GetAddUsuarioSoftv(Parametros).then(function (data) {
+       vm.IdUser=data.GetAddUsuarioSoftvResult.Clave;
+      });
+
+
+      /*vm.Clave
+      vm.Nombre
+      vm.rol.IdRol
+      vm.pass2
+      vm.pass1
+      vm.identificacion
+      vm.fechaingreso
+      vm.fechabaja
+      vm.activo
+      vm.recibemensaje*/
+    }
+
 
     var vm = this;
-
+    vm.Guardar = Guardar;
+    vm.muestraplazas = muestraplazas;
+    vm.agregaRelacion=agregaRelacion;
+    vm.IdUser=0;
 
   });
--- app/scripts/factories/usuarioFactory.js
+++ app/scripts/factories/usuarioFactory.js
@@ -3,8 +3,134 @@ angular.module('softvApp')
   .factory('usuarioFactory', function ($http, $q, globalService, $localStorage) {
     var factory = {};
     var paths = {
-      GetUsuarioSoftvList: '/Usuario/GetUsuarioSoftvList'
+      GetUsuarioSoftvList: '/Usuario/GetUsuarioSoftvList',
+      GetConsultaIdentificacionUsuario: '/Usuario/GetConsultaIdentificacionUsuario',
+      GetAddUsuarioSoftv: '/Usuario/GetAddUsuarioSoftv',
+      GetAgregaEliminaRelCompaniaUsuario: '/Usuario/GetAgregaEliminaRelCompaniaUsuario',
+      GetSoftvweb_GetUsuarioSoftvbyId: '/Usuario/GetSoftvweb_GetUsuarioSoftvbyId'
     };
+
+
+
+
+    factory.GetAgregaEliminaRelCompaniaUsuario = function (clv_usuario, idcompania, opcion) {
+      var deferred = $q.defer();
+      var config = {
+        headers: {
+          'Authorization': $localStorage.currentUser.token
+        }
+      };
+      var Parametros = {
+        'clv_usuario': clv_usuario,
+        'idcompania': idcompania,
+        'opcion': opcion
+      };
+      console.log(Parametros);
+      $http.post(globalService.getUrl() + paths.GetAgregaEliminaRelCompaniaUsuario, JSON.stringify(Parametros), config).then(function (response) {
+        deferred.resolve(response.data);
+      }).catch(function (response) {
+        deferred.reject(response);
+      });
+      return deferred.promise;
+    };
+
+
+    factory.GetSoftvweb_GetUsuarioSoftvbyId = function (Id) {
+      var deferred = $q.defer();
+      var config = {
+        headers: {
+          'Authorization': $localStorage.currentUser.token
+        }
+      };
+      var Parametros = {
+        'Id': Id,
+
+      };
+      console.log(Parametros);
+      $http.post(globalService.getUrl() + paths.GetSoftvweb_GetUsuarioSoftvbyId, JSON.stringify(Parametros), config).then(function (response) {
+        deferred.resolve(response.data);
+      }).catch(function (response) {
+        deferred.reject(response);
+      });
+      return deferred.promise;
+    };
+
+
+
+
+
+
+
+
+
+    factory.GetAddUsuarioSoftv = function (object) {
+      var deferred = $q.defer();
+      var config = {
+        headers: {
+          'Authorization': $localStorage.currentUser.token
+        }
+      };
+      var Parametros = {
+        'usuario': {
+          'Clave': 0,
+          'Clv_Usuario': object.Clv_Usuario,
+          'Domicilio': object.Domicilio,
+          'Colonia': object.Colonia,
+          'FechaIngreso': object.FechaIngreso,
+          'FechaSalida': object.FechaSalida,
+          'Activo': object.Activo,
+          'Pasaporte': object.Pasaporte,
+          'Clv_TipoUsuario': object.Clv_TipoUsuario,
+          'CATV': object.CATV,
+          'Facturacion': object.Facturacion,
+          'Boletos': object.Boletos,
+          'Mizar_AN': object.Mizar_AN,
+          'RecibeMensaje': object.RecibeMensaje,
+          'NotaDeCredito': object.NotaDeCredito,
+          'Clv_IdentificacionUsuario': object.Clv_IdentificacionUsuario,
+          'RecibeMensajeDocumentos': object.RecibeMensajeDocumentos,
+          'Nombre': object.Nombre
+
+
+        }
+
+      };
+      console.log(Parametros);
+      $http.post(globalService.getUrl() + paths.GetAddUsuarioSoftv, JSON.stringify(Parametros), config).then(function (response) {
+        deferred.resolve(response.data);
+      }).catch(function (response) {
+        deferred.reject(response);
+      });
+      return deferred.promise;
+    };
+
+
+
+
+
+    factory.GetConsultaIdentificacionUsuario = function (clave, Nombre) {
+      var deferred = $q.defer();
+      var config = {
+        headers: {
+          'Authorization': $localStorage.currentUser.token
+        }
+      };
+      var Parametros = {
+        'clave': clave,
+        'Nombre': Nombre
+      };
+      console.log(Parametros);
+      $http.post(globalService.getUrl() + paths.GetConsultaIdentificacionUsuario, JSON.stringify(Parametros), config).then(function (response) {
+        deferred.resolve(response.data);
+      }).catch(function (response) {
+        deferred.reject(response);
+      });
+      return deferred.promise;
+    };
+
+
+
+
     factory.GetRolList = function () {
       var deferred = $q.defer();
       var config = {
@@ -21,7 +147,7 @@ angular.module('softvApp')
     };
 
 
-    factory.GetUsuarioSoftvList = function (ClvUsuario, Nombre, Op, idcompania) {
+    factory.GetUsuarioSoftvList = function (object) {
       var deferred = $q.defer();
       var config = {
         headers: {
@@ -29,13 +155,13 @@ angular.module('softvApp')
         }
       };
       var Parametros = {
-        'Clv_Usuario': ClvUsuario,
-        'Nombre': Nombre,
-        'Op': Op,
-        'idcompania': idcompania
-      }
-
-      $http.post(globalService.getUrl() + paths.GetUsuarioSoftvList, config, JSON.stringify(Parametros)).then(function (response) {
+        'Clv_Usuario': object.ClvUsuario,
+        'Nombre': object.Nombre,
+        'Op': object.Op,
+        'idcompania': object.idcompania
+      };
+      console.log(Parametros);
+      $http.post(globalService.getUrl() + paths.GetUsuarioSoftvList, JSON.stringify(Parametros), config).then(function (response) {
         deferred.resolve(response.data);
       }).catch(function (response) {
         deferred.reject(response);
