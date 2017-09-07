a34d035730674ba085eeacbe030fa53a759eb327
EricChavez <erictics@gmail.com>
2017-09-07 11:21:39 -0500

    cambios a formulario de quejas y atencion

--- app/scripts/controllers/configuracion/modules.js
+++ app/scripts/controllers/configuracion/modules.js
@@ -1,132 +1,132 @@
 'use strict';
 angular
-	.module('softvApp')
-	.config(function($stateProvider) {
-		var states = [{
-				name: 'home.configuracion',
-				abstract: true,
-				template: '<div ui-view></div>'
-			},
-			{
-				name: 'home.configuracion.permisos',
-				data: {
-					pageTitle: 'SAC | PERMISOS',
-					permissions: {
-						only: ['permisosSelect'],
-						options: {
-							reload: false
-						}
-					}
-				},
-				url: '/configuracion/permisos',
-				templateUrl: 'views/configuracion/permisos.html',
-				controller: 'PermisosCtrl',
-				controllerAs: '$ctrl'
-			},
-			{
-				name: 'home.configuracion.roles',
-				data: {
-					pageTitle: 'SAC | ROLES',
-					permissions: {
-						only: ['rolesSelect'],
-						options: {
-							reload: false
-						}
-					}
-				},
-				url: '/configuracion/roles',
-				templateUrl: 'views/configuracion/roles.html',
-				controller: 'RolCtrl',
-				controllerAs: '$ctrl'
-			},
-			{
-				name: 'home.configuracion.usuarios',
-				data: {
-					pageTitle: 'SAC | ROLES',
-					permissions: {
-						only: ['usuariosSelect'],
-						options: {
-							reload: false
-						}
-					}
-				},
-				url: '/configuracion/usuarios',
-				templateUrl: 'views/configuracion/usuarios.html',
-				controller: 'usuariosCtrl',
-				controllerAs: '$ctrl'
-			},
-			{
-				name: 'home.configuracion.nuevousuario',
-				data: {
-					pageTitle: 'SAC | USUARIOS',
-					permissions: {
-						only: ['usuariosSelect'],
-						options: {
-							reload: false
-						}
-					}
-				},
-				url: '/configuracion/nuevousuario',
-				templateUrl: 'views/configuracion/nuevoUsuario.html',
-				controller: 'nuevoUsuarioCtrl',
-				controllerAs: '$ctrl'
-			},
-			{
-				name: 'home.configuracion.detalleusuario',
-				data: {
-					pageTitle: 'SAC | DETALLE USUARIOS',
-					permissions: {
-						only: ['usuariosSelect'],
-						options: {
-							reload: false
-						}
-					}
-				},
-				url: '/configuracion/detalleusuario/:id',
-				templateUrl: 'views/configuracion/nuevoUsuario.html',
-				controller: 'detalleUsuarioCtrl',
-				controllerAs: '$ctrl'
-			},
-			{
-				name: 'home.configuracion.editausuario',
-				data: {
-					pageTitle: 'SAC | EDITA USUARIOS',
-					permissions: {
-						only: ['usuariosSelect'],
-						options: {
-							reload: false
-						}
-					}
-				},
-				url: '/configuracion/editausuario/:id',
-				templateUrl: 'views/configuracion/nuevoUsuario.html',
-				controller: 'editaUsuarioCtrl',
-				controllerAs: '$ctrl'
-			},
-{
-    name: 'home.configuracion.rolesweb',
-    data: {
-     pageTitle: 'SAC | ROLES WEB',
-     permissions: {
-      //only: ['permisosSelect'],
-      options: {
-       reload: false
+  .module('softvApp')
+  .config(function ($stateProvider) {
+    var states = [{
+        name: 'home.configuracion',
+        abstract: true,
+        template: '<div ui-view></div>'
+      },
+      {
+        name: 'home.configuracion.permisos',
+        data: {
+          pageTitle: 'SAC | PERMISOS',
+          permissions: {
+            only: ['permisosSelect'],
+            options: {
+              reload: false
+            }
+          }
+        },
+        url: '/configuracion/permisos',
+        templateUrl: 'views/configuracion/permisos.html',
+        controller: 'PermisosCtrl',
+        controllerAs: '$ctrl'
+      },
+      {
+        name: 'home.configuracion.roles',
+        data: {
+          pageTitle: 'SAC | ROLES',
+          permissions: {
+            only: ['rolesSelect'],
+            options: {
+              reload: false
+            }
+          }
+        },
+        url: '/configuracion/roles',
+        templateUrl: 'views/configuracion/roles.html',
+        controller: 'RolCtrl',
+        controllerAs: '$ctrl'
+      },
+      {
+        name: 'home.configuracion.usuarios',
+        data: {
+          pageTitle: 'SAC | ROLES',
+          permissions: {
+            only: ['usuariosSelect'],
+            options: {
+              reload: false
+            }
+          }
+        },
+        url: '/configuracion/usuarios',
+        templateUrl: 'views/configuracion/usuarios.html',
+        controller: 'usuariosCtrl',
+        controllerAs: '$ctrl'
+      },
+      {
+        name: 'home.configuracion.nuevousuario',
+        data: {
+          pageTitle: 'SAC | USUARIOS',
+          permissions: {
+            only: ['usuariosSelect'],
+            options: {
+              reload: false
+            }
+          }
+        },
+        url: '/configuracion/nuevousuario',
+        templateUrl: 'views/configuracion/nuevoUsuario.html',
+        controller: 'nuevoUsuarioCtrl',
+        controllerAs: '$ctrl'
+      },
+      {
+        name: 'home.configuracion.detalleusuario',
+        data: {
+          pageTitle: 'SAC | DETALLE USUARIOS',
+          permissions: {
+            only: ['usuariosSelect'],
+            options: {
+              reload: false
+            }
+          }
+        },
+        url: '/configuracion/detalleusuario/:id',
+        templateUrl: 'views/configuracion/nuevoUsuario.html',
+        controller: 'detalleUsuarioCtrl',
+        controllerAs: '$ctrl'
+      },
+      {
+        name: 'home.configuracion.editausuario',
+        data: {
+          pageTitle: 'SAC | EDITA USUARIOS',
+          permissions: {
+            only: ['usuariosSelect'],
+            options: {
+              reload: false
+            }
+          }
+        },
+        url: '/configuracion/editausuario/:id',
+        templateUrl: 'views/configuracion/nuevoUsuario.html',
+        controller: 'editaUsuarioCtrl',
+        controllerAs: '$ctrl'
+      },
+      {
+        name: 'home.configuracion.rolesweb',
+        data: {
+          pageTitle: 'SAC | ROLES WEB',
+          permissions: {
+            //only: ['permisosSelect'],
+            options: {
+              reload: false
+            }
+          }
+        },
+        url: '/configuracion/roles_lista',
+        templateUrl: 'views/configuracion/RolesWeb.html',
+        controller: 'RolesWebCtrl',
+        controllerAs: '$ctrl'
       }
-     }
-    },
-    url: '/configuracion/roles_lista',
-    templateUrl: 'views/configuracion/RolesWeb.html',
-    controller: 'RolesWebCtrl',
-    controllerAs: '$ctrl'
-   }
 
 
 
-			
 
 
-		];
-		states.forEach(function(state) {
-			$stateProvider.state(state);
-		});
-	});
+
+    ];
+    states.forEach(function (state) {
+      $stateProvider.state(state);
+    });
+  });
